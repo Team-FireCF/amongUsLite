86b912463ecf8ae60f813a4a8cbab0edb799a34b
Garhett Morgan <32fossil@gmail.com>
2020-10-09 11:58:30 -0700

    initial commit

---
+++ src/main/java/teamFire/JavaMongUs/JavaMongUsApplication.java
@@ -0,0 +1,13 @@
+package teamFire.JavaMongUs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMongUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMongUsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teamFire/JavaMongUs/JavaMongUsApplicationTests.java
@@ -0,0 +1,13 @@
+package teamFire.JavaMongUs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMongUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
