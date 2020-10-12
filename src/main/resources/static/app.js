31cd2dedf08427c4d648f05280d335d9ccee48a3
claudiobailon <c.bailonschubert@gmail.com>
2020-10-12 16:39:56 -0700

    Working on messages

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,49 @@
+var stompClient=null;
+
+
+function setConnected(connected) {
+  $("#connect").prop("disabled", connected);
+  $("#disconnect").prop("disabled", !connected);
+  if (connected) {
+      $("#conversation").show();
+  }
+  else {
+      $("#conversation").hide();
+  }
+  $("#messages").html("");
+}
+
+function connect(){
+console.log("Testing");
+  var socket =  new SockJS('/javaMongUs');
+  stompClient = Stomp.over(socket);
+  stompClient.connect({}, function(frame){
+    // setConnected(true); TODO:Write setConnected function if needed
+    console.log("connected " + frame);
+    stompClient.subscribe('/game/messages', function(message){
+      showMessage(JSON.parse(message.body).content);
+    });//TODO:Stretch: Make messages dynamic
+
+  })
+};
+
+function disconnect(){
+  if(stompClient !== null){
+    stompClient.disconnect();
+  }
+  setConnected(false);
+  console.log("Disconnected");
+}
+
+function sendMessage(){
+  stompClient.send("/app/userTexts", {}, JSON.stringify({'message':$("#text")}))
+}
+
+$(function () {
+  $("form").on('submit', function (e) {
+      e.preventDefault();
+  });
+  $( "#connect" ).click(function() { connect(); });
+  $( "#disconnect" ).click(function() { disconnect(); });
+  $( "#send" ).click(function() { sendName(); });
+});
